82daf92ba96e325a61b49ce31baf8593265e038c
nicklemke02 <nicklemke02@gmail.com>
2024-06-13 20:08:58 -0500

    file revisions

---
+++ 3_sql_project/1_higest_paid_jobs.sql
@@ -0,0 +1,17 @@
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name as company_name
+FROM job_postings_fact
+LEFT JOIN company_dim
+ON company_dim.company_id = job_postings_fact.company_id
+WHERE
+    job_location = 'Anywhere'
+    AND job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
